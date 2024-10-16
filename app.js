06f6661d2f03d9b7cee92cc2dc0df90028bf357f
Wade Wegner <wade.wegner@gmail.com>
2024-10-16 12:54:53 -0700

    fixed port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
